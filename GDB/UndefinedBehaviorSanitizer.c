50050a1ecf6fdcbe4524bc5bac72164c01f10429
Ryoichi Kakehi <ryoichikakehi@gmail.com>
2023-04-18 23:37:00 +0900

    new file:   HowToCompile_UndefinedBehaviorSanitizer.txt 	new file:   UndefinedBehaviorSanitizer.c

---
+++ GDB/UndefinedBehaviorSanitizer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv)
+{
+    if (strcmp(argv[1], "foo") == 0)
+    {
+        printf("foo!\n");
+    }
+    else 
+    {
+        printf("hello\n");
+    }
+
+    return 0;
+}
